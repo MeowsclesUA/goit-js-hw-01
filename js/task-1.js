e624dbb630c81374fcad9c1c91c959648503550c
İvan <kliuievivan@gmail.com>
2025-04-05 17:57:51 +0300

    added js

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const quantity = null;
+const pricePerDroid = null;
+makeTransaction(quantity, pricePerDroid);
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+const country = null;
+const price = null;
+const deliveryFee = null;
+getShippingMessage(country, price, deliveryFee);
+console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20)); 
